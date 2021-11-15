e1b1bf786aad553cddfbbeb3affff82526b0b467
dotnotation <dotinmotionproductions@gmail.com>
2021-11-15 12:46:03 -0500

    commit tree 44866502be4fbd98c8d5a88a432b9a0ee2452d81

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const heartButton = document.querySelectorAll(".like-glyph");
+const errorMessage = document.getElementById("modal");
 
 // Your JavaScript code goes here!
+for (const heart of heartButton) {
+  heart.addEventListener("click", fillHeart);
+}
 
+function fillHeart(e){
+  e.preventDefault();
+  const hearts = e.target;
+  console.log(e.target);
 
+  mimicServerCall("url")
+    .then((response) => {
+      errorMessage.innerText = response;
+      errorMessage.classList.remove("hidden");
+      setTimeout(() => {errorMessage.classList.add("hidden")}, 3000);
+      if (e.target.innerText == EMPTY_HEART) {
+        e.target.innerText = FULL_HEART;
+        e.target.classList.add("activated-heart");
+      } else {
+        e.target.innerText == EMPTY_HEART;
+        e.target.classList.remove("activated-heart");
+      }
+    })
+    .catch((error) => {
+      errorMessage.innerText = error;
+      errorMessage.classList.remove("hidden");
+      setTimeout(() => {errorMessage.classList.add("hidden")}, 3000);
+    })
+}
+ 
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +52,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+// returns either a success of fail response at random
+// mocks a backend server
